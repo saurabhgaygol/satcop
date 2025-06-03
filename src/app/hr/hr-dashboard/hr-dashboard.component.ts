f0ece754519211354669273e10a1b2bdae57868b
Saurabh Gaygol <saurabhgaygol369@gmail.com>
2025-06-03 17:00:22 +0530

    oferleter upadated And change password updated

--- src/app/hr/hr-dashboard/hr-dashboard.component.ts
+++ src/app/hr/hr-dashboard/hr-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { NotificationComponent } from "../../part/notification/notification.component";
 import { CommonModule } from '@angular/common';
@@ -8,43 +8,64 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-hr-dashboard',
   standalone: true,
-  imports: [RouterLink, RouterModule, NotificationComponent,CommonModule],
+  imports: [RouterLink, RouterModule, NotificationComponent, CommonModule],
   templateUrl: './hr-dashboard.component.html',
   styleUrls: ['./hr-dashboard.component.css']
 })
 export class HrDashboardComponent implements OnInit {
+
+  //veriable
   showNotification = false;
   userName: any;
+  showchangepassword = false;
 
-  constructor(private router: Router) {}
 
-   ngOnInit(): void {
+
+  //constracter
+  constructor(private router: Router) { }
+
+  //method to show user longin
+  ngOnInit(): void {
     const data = sessionStorage.getItem('userdata');
     if (data) {
       const userObj = JSON.parse(data);
       this.userName = userObj.userId;
-      
+
     }
   }
 
-    
 
+  //notification logic show
+  toggleNotification() {
+    this.showNotification = !this.showNotification;
 
- 
-
-  toggleNotification() {//ye notification ki method hai
-   this.showNotification = !this.showNotification;
-  
   }
 
+  //notification logic close
   closeNotification() {
-  this.showNotification = false;
-}
+    this.showNotification = false;
+  }
 
-
+  // method to logout page
   signOut() {
-  sessionStorage.clear();
-  localStorage.clear();
-  this.router.navigate(['/login']);
-    }
+    sessionStorage.clear();
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
+
+
+
+  openModal() {
+    const modalElement = document.getElementById('changePasswordModal');
+    const modal = new (window as any).bootstrap.Modal(modalElement);
+    modal.show();
+  }
+
 }
+
+
+
+
+
+
+
